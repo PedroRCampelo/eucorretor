cf86ab2e0beb06a26390365c61f90d8382b22878
PedroRCampelo <pedrocampelo@live.com>
2025-03-13 18:07:11 -0300

    firsts connections with db and CRUD

---
+++ src/main/java/com/eucorretor/controller/CustomerController.java
@@ -0,0 +1,49 @@
+package com.eucorretor.controller;
+
+import com.eucorretor.model.Customer;
+import com.eucorretor.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    
+    @Autowired
+    private CustomerRepository customerRepository;
+    
+    // List customers
+    @GetMapping
+    public List<Customer> listCustomers() {
+        return customerRepository.findAll();
+    }
+
+    // Search customer based on id
+    @GetMapping("/{id}")
+    public Optional<Customer> findCustomers(@PathVariable Long id) {
+        return customerRepository.findById(id);
+    }
+
+    // Create new customer
+    @PostMapping
+    public Customer createCustomers(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    // Upd customer
+    @PutMapping("/{id}")
+    public Customer updCustomer(@PathVariable Long id, @RequestBody Customer updatedCustomer) {
+        return customerRepository.findById(id).map(customer -> {
+            customer.setName(updatedCustomer.getName());
+            customer.setEmail(updatedCustomer.getEmail());
+            customer.setPhone(updatedCustomer.getPhone());
+            return customerRepository.save(customer);
+        }).orElseThrow(() -> new RuntimeException("Failed to find the id"));
+    }
+    
+    
+
+}
---
+++ src/main/java/com/eucorretor/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.eucorretor.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.eucorretor.model.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
