df3c461dd4a6dda96f90608449f7b8bba591acb8
PedroRCampelo <pedrocampelo@live.com>
2025-03-20 15:55:30 -0300

    creating new customer information on backend

--- src/main/java/com/eucorretor/model/Customer.java
+++ src/main/java/com/eucorretor/model/Customer.java
@@ -4,12 +4,13 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 
 @Entity
 @Table(name="CUSTOMERS")
 
 public class Customer {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter
@@ -25,5 +26,31 @@ public class Customer {
     @Setter
     @Getter
     private String phone;
-
+    @Setter
+    @Getter
+    private Date birthday;
+    @Setter
+    @Getter
+    private Date validity;
+    @Setter
+    @Getter
+    private String veicPlate;
+    @Setter
+    @Getter
+    private String veicModel;
+    @Setter
+    @Getter
+    private String company;
+    @Setter
+    @Getter
+    private String installment;
+    @Setter
+    @Getter
+    private String award;
+    @Setter
+    @Getter
+    private String endosso;
+    @Setter
+    @Getter
+    private boolean active;
 }
